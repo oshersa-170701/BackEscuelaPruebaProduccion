a48b33eb2faf11276170e89bd94293f683777403
Ale <alex1313shi@gmail.com>
2025-07-10 09:37:46 -0600

    datos de conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GruposModule } from './grupos/grupos.module';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mariadb',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'escuelaDB',
+    synchronize: true,
+    autoLoadEntities: true,
+  }), GruposModule],
   controllers: [AppController],
   providers: [AppService],
 })
