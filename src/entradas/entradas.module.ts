a823aa5b634baf04d6a740299a4ba0134b867ff2
Abejandroo <alejandro7521x@gmail.com>
2025-07-15 09:35:26 -0600

    Creacion de la tabla de entradas

--- src/entradas/entradas.module.ts
+++ src/entradas/entradas.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EntradasService } from './entradas.service';
 import { EntradasController } from './entradas.controller';
+import { Entrada } from './entities/entrada.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Entrada])],
   controllers: [EntradasController],
   providers: [EntradasService],
 })
