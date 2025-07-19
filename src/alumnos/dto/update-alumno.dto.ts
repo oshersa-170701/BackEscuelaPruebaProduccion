50d4ba8895bce911707c5ae2be1b2814d9e79961
Ale <alex1313shi@gmail.com>
2025-07-19 13:15:54 -0600

    crud de alumnos

--- src/alumnos/dto/update-alumno.dto.ts
+++ src/alumnos/dto/update-alumno.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlumnoDto } from './create-alumno.dto';
+import { IsOptional, IsString } from 'class-validator';
+import { Grupochido } from 'src/grupos/entities/grupo.entity';
+import { Tutore } from 'src/tutores/entities/tutore.entity';
 
-export class UpdateAlumnoDto extends PartialType(CreateAlumnoDto) {}
+export class UpdateAlumnoDto  {
+    @IsString()
+    @IsOptional()
+    nombre: string;
+
+    @IsString()
+    @IsOptional()
+    apellidoP: string;
+
+    @IsString()
+    @IsOptional()
+    apellidoM: string;
+
+    @IsOptional()
+    tutor:Tutore;
+    @IsOptional()
+    grupo:Grupochido;
+}
