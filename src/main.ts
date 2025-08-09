1e744bbaf41d50a60addcb77d60c8dce3265aebe
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-08-09 17:12:26 -0600

    SE REFINÓ LA LÓGICA DE INICIO DEL SERVIDOR AL ASIGNAR EL PUERTO A UNA VARIABLE ANTES DE ESCUCHAR

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
