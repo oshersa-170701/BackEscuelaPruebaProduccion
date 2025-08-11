078399f8cc895e88ba5e163747a627ca29d7f779
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-08-11 16:30:03 -0600

    sds

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
